15691f59eb76239c50bf0f7da7eca5548087fcc4
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-02-14 02:41:35 -0500

    adding finished pratice typescript

--- typescript.js
+++ typescript.js
@@ -22,3 +22,83 @@ var items = {
 // null/undefined
 var undefexample = undefined;
 var nullexample = null;
+//tuple 
+var basket;
+basket = ['baskball', 4];
+//Enumerable
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size[2]; // -> Medium
+var sizeName2 = Size.Small; // -> 1
+// Any (can have any type) will never throw out and error like JS
+// Good for converting JS to TS for really complicated functions that
+// can't be easily defined at the start
+var something = 'Anything can be here';
+something = basket;
+// void -> expect no return
+var sing = function () {
+    console.log('void');
+};
+var sing2 = function () {
+    return 'string';
+};
+// never -> tests if the function has no return type 
+// and if it has a unreachable endpoint 
+var error = function () {
+    throw Error('something went wrong');
+};
+var fightRobotArmyQuestionMark = function (robots) {
+    console.log('Fighting');
+};
+// want robots to have a countm type and magic of their respective
+// types
+var fightRobotArmy = function (robots) {
+    console.log('Fighting');
+};
+// Same as above 
+var fightRobotArmy2 = function (robots) {
+    console.log('Fighting');
+};
+var dog = {};
+dog.count; // -> works if you do let dog = {} as CatArmy
+//function
+var fightRobotArmyFunction = function (robots) {
+    console.log('Fighting');
+};
+var fightRobotArmyFunction2 = function (robots) {
+    console.log('Fighting');
+    return 5;
+};
+// Class
+var Animal = /** @class */ (function () {
+    function Animal(sound) {
+        this.sing = 'lalala';
+        this.sing = sound;
+    }
+    Animal.prototype.greet = function () {
+        return "'Hello ".concat(this.sing);
+    };
+    return Animal;
+}());
+var lion = new Animal('roar');
+lion.greet();
+// Get error because you are accessing a private variable
+// class Animal {
+//     private sing: string = 'lalala'
+//     constructor(sound: string){
+//         this.sing = sound;
+//     }
+//     greet() {
+//         return `'Hello ${this.sing}`
+//     }
+// }
+// let lion = new Animal('roar')
+// lion.greet();
+// Union type
+var confused = "hello";
+var confused2 = 8;
+// let confused3: string | number = false   --> error
--- typescript.ts
+++ typescript.ts
@@ -37,3 +37,128 @@ let items: object = {
 
 let undefexample: undefined = undefined;
 let nullexample: null = null;
+
+
+//tuple 
+
+let basket: [string, number];
+basket = ['baskball', 4]
+
+//Enumerable
+
+enum Size {Small = 1, Medium = 2, Large = 3}
+let sizeName: string = Size[2] // -> Medium
+let sizeName2: number = Size.Small // -> 1
+
+// Any (can have any type) will never throw out and error like JS
+// Good for converting JS to TS for really complicated functions that
+// can't be easily defined at the start
+let something: any = 'Anything can be here'
+something = basket
+
+
+// void -> expect no return
+let sing = (): void => {
+    console.log('void');
+}
+
+let sing2 = (): string => {
+    return 'string';
+}
+
+// never -> tests if the function has no return type 
+// and if it has a unreachable endpoint 
+let error = (): never =>{
+    throw Error('something went wrong')
+}
+
+// Interface
+// creates a new name that can be used everywhere 
+interface RobotArmy {
+    count: number,
+    type: string,
+     magic?: string // -> ? means it may or may not be apart of the
+                    // interface
+}
+
+let fightRobotArmyQuestionMark = (robots: {count: 2, type: 'dragon'}) => {
+    console.log('Fighting')
+}
+// want robots to have a countm type and magic of their respective
+// types
+let fightRobotArmy = (robots: RobotArmy) => {
+    console.log('Fighting')
+}
+// Same as above 
+let fightRobotArmy2 = (robots: {count: number
+                        ,type: string, magic: string}) => {
+    console.log('Fighting')
+}
+
+// type doesn't create a new name
+type RobotArmy2 ={
+    count: number,
+    type: string,
+     magic: string
+}
+
+// Type assertion (dangerous to use)
+interface CatArmy {
+    count: number,
+    type: string,
+     magic: string
+}
+
+let dog = {} as CatArmy
+
+dog.count // -> works if you do let dog = {} as CatArmy
+
+
+
+//function
+
+let fightRobotArmyFunction = (robots: RobotArmy): void => {
+    console.log('Fighting')
+}
+
+let fightRobotArmyFunction2 = (robots: RobotArmy): number => {
+    console.log('Fighting')
+    return 5;
+}
+
+// Class
+class Animal {
+    sing: string = 'lalala'
+    constructor(sound: string){
+        this.sing = sound;
+    }
+    greet() {
+        return `'Hello ${this.sing}`
+    }
+}
+
+let lion = new Animal('roar')
+lion.greet();
+
+// Get error because you are accessing a private variable
+
+// class Animal {
+//     private sing: string = 'lalala'
+//     constructor(sound: string){
+//         this.sing = sound;
+//     }
+//     greet() {
+//         return `'Hello ${this.sing}`
+//     }
+// }
+
+// let lion = new Animal('roar')
+// lion.greet();
+
+
+// Union type
+
+let confused: string | number = "hello"
+let confused2: string | number = 8
+// let confused3: string | number = false   --> error
+
