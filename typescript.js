271c08caa28912b729d0d041a43ad9a2e21a6d1e
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-02-14 01:29:50 -0500

    adding pratice typescript

---
+++ typescript.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+// const sum = (a : number, b : number) =>{
+//     return a + b;
+// }
+// sum(4,5);
+// boolean
+var isCool = true;
+//number
+var weight = 150;
+//string
+var color = 'red';
+var quote = "my favorite color is".concat(color);
+//array
+var pets = ['cat', 'dog', 'cow'];
+var pets2 = ['cat', 'dog', 'cow'];
+//Object
+var items = {
+    a: 'pan',
+    b: 'bottle'
+};
+// null/undefined
+var undefexample = undefined;
+var nullexample = null;
---
+++ typescript.ts
@@ -0,0 +1,39 @@
+// so that cab't redcleare block-scoped variable doesn't happen
+export {} 
+// const sum = (a : number, b : number) =>{
+//     return a + b;
+// }
+
+// sum(4,5);
+
+// boolean
+let isCool: boolean=  true;
+
+//number
+
+let weight: number = 150;
+
+//string
+
+let color: string = 'red';
+
+let quote: string = `my favorite color is${color}`
+
+
+//array
+
+let pets: string[] = ['cat', 'dog', 'cow']
+
+let pets2: Array<string> = ['cat', 'dog', 'cow']
+
+//Object
+
+let items: object = {
+    a: 'pan', 
+    b: 'bottle'
+}
+
+// null/undefined
+
+let undefexample: undefined = undefined;
+let nullexample: null = null;
